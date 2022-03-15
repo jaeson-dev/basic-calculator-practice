bf3d636af3005f48ec3c4a4126b30d4929742ef2
jaeson-dev <jaeson.dev@gmail.com>
2022-03-15 16:39:51 +0900

    Initial Commit

---
+++ MyCalculator_practice/MyCalculator_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculator_practiceApp.swift
+//  MyCalculator_practice
+//
+//  Created by Jaeson.dev on 2022/03/15.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculator_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
